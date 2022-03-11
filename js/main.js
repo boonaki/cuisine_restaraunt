35c90ef4e7e59422e6edd487ba97932603427c71
boonaki <91851563+boonaki@users.noreply.github.com>
2022-03-10 22:16:10 -0500

    Add files via upload

--- js/main.js
+++ js/main.js
@@ -1,1 +1,15 @@
+document.querySelector("#hamburg").addEventListener('click', toggleSideBarOn)
+document.querySelector("#hamburg2").addEventListener('click', toggleSideBarOff)
 
+
+function toggleSideBarOn(){
+    document.querySelector("#navigation").classList.toggle('target')
+    document.querySelector('#hamburg').classList.add('hidden')
+    document.querySelector('#hamburg2').classList.remove('hidden')
+}
+
+function toggleSideBarOff(){
+    document.querySelector("#navigation").classList.toggle('target')
+    document.querySelector('#hamburg2').classList.add('hidden')
+    document.querySelector('#hamburg').classList.remove('hidden')
+}
